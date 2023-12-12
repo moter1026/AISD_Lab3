3530deb4e417d6c96e0dba84e064aafd74cb3f83
moter1026 <76918726+moter1026@users.noreply.github.com>
2023-12-13 02:07:29 +0400

    Update AISD Lab3.cpp

--- AISD Lab3.cpp
+++ AISD Lab3.cpp
@@ -7,17 +7,6 @@ using namespace std;
 
 int main()
 {
-	vector<int> vec;
-	vec.push_back(2);
-	vec.push_back(5);
-	vec.push_back(3);
-	vec.push_back(7);
-	vec.push_back(9);
-	vec.push_back(50);
-	vec.push_back(1);
-	vec.push_back(2);
-	vec.push_back(34);
-
 	{
 		vector<int> vec;
 		vec.push_back(2);
